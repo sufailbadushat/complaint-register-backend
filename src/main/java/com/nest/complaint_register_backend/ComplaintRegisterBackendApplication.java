2c8aea23b248a05d7f7760824c4207a5b4af4a8b
sufailbadushat <sufailbadushat@gamil.com>
2022-12-09 15:15:53 +0530

    first commit

---
+++ src/main/java/com/nest/complaint_register_backend/ComplaintRegisterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.complaint_register_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintRegisterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintRegisterBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nest/complaint_register_backend/ComplaintRegisterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.complaint_register_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintRegisterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
